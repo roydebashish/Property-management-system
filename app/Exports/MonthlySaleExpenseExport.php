7f42a76ef47a15d5f21363f3c67874698ffb2cc2
Debashish Roy <roy.debashish@outlook.com>
2019-10-19 12:45:07 +0600

    monthly sales & expense & gross profit excel done

--- app/Exports/MonthlySaleExpenseExport.php
+++ app/Exports/MonthlySaleExpenseExport.php
@@ -23,23 +23,37 @@ class MonthlySaleExpenseExport implements FromCollection, WithMapping, WithHeadi
     {
         $counter = Carbon::now()->daysInMonth;
         $data = [];
+        $monthly_total_sale = 0;
+        $monthly_total_expense = 0;
         for($i = 1; $i <= $counter; $i++)
         {
             $i = ($i < 10) ? '0'.$i : $i;
             $date = Carbon::now()->year.'-'.Carbon::now()->month.'-'.$i;
-            $expense = Expense::whereDate('expense_date', $date)->get();
-            //$total_expense = Helper::calulate_total_expense($expense);
-            dd($expense);
+            $expense = Expense::whereDate('expense_date', $date)->pluck('expense');
+            $total_expense = Helper::calulate_total_expense($expense);
             $sales = Sale::whereDate('created_at', $date)->sum('sale_amount');
+            $monthly_total_sale += $sales;
+            $monthly_total_expense += $total_expense;
             $data[] = [
                 'date' => $date,
                 'sale' => $sales,
                 'expense' => $total_expense
             ];
         }
-        
+        #prepare monthly total sales & Expense
+        $data[] = [
+            'date' => 'Total =',
+            'sale' => $monthly_total_sale,
+            'expense' => $monthly_total_expense
+        ];
+        #pepare gross profit
+         $data[] = [
+            'date' => 'Gross Profit =',
+            'sale' => $monthly_total_sale - $monthly_total_expense,
+            'expense' => ''
+        ];
+
         return collect($data);
-        
     }
 
     /** Headings*/
